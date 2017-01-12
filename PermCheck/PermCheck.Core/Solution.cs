38e9873c40b8bed84bd92b3856ef21501740130c
slanto <slawek.antos@outlook.com>
2017-01-12 16:44:11 +0100

    Solution with tests added

--- PermCheck/PermCheck.Core/Solution.cs
+++ PermCheck/PermCheck.Core/Solution.cs
@@ -1,10 +1,34 @@
 ﻿namespace PermCheck.Core
 {
+    using System.Collections.Generic;
+
     public class Solution
     {
-        public int IsArrayPermutation(int[] a)
+        public int IsArrayPermutation(int[] array)
         {
-            return 1;
+            var max = 0;
+            var list = new List<int>();
+
+            foreach (var elem in array)
+            {
+                list.Add(elem);
+                if (elem > max)
+                {
+                    max = elem;
+                }
+            }
+
+            var isPerm = 1;
+            for (var i = 1; i <= max; i++)
+            {
+                if (!list.Contains(i))
+                {
+                    isPerm = 0;
+                    break;                    
+                }
+            }
+
+            return isPerm;
         }        
     }
 }
--- PermCheck/PermCheck.Tests/SolutionTest.cs
+++ PermCheck/PermCheck.Tests/SolutionTest.cs
@@ -8,6 +8,7 @@
     {
         [Theory]
         [InlineData(new[] { 4, 1, 3, 2}, 1)]
+        [InlineData(new[] { 4, 1, 3 }, 0)]
         public void IsArrayPermutation_ReturnsCorrectValueForGivenArray(int[] array, int expected)
         {
             var sut = new Solution();
